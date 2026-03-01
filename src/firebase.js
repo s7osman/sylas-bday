02bebb8a580fc01c4b03a087205070655faadb03
Suban <subanosman1@gmail.com>
2026-03-01 02:05:27 -0500

    updated db config

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDkZFX7g12uOTLEgN5K5A-XQ_vfhIApHks',
-  authDomain: 'sylas-bday.firebaseapp.com',
-  projectId: 'sylas-bday',
-  storageBucket: 'sylas-bday.firebasestorage.app',
-  messagingSenderId: '258607998843',
-  appId: '1:258607998843:web:39a52199e229571344ffde',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
